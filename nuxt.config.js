7bc510947659389aaa804724391bd1dca9f27519
herlanmustopa <hrmustopa.dev@gmail.com>
2022-04-14 12:22:20 +0700

    dist

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,7 +51,7 @@ export default {
     port: 3030,
   },
   target: 'serverless', // default is 'server'
-
+  ssr: false,
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
